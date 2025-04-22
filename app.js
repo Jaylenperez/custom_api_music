038649899860905929bf8f3084e43accdb7a26cb
jaylenperez <jaylenestella24@gmail.com>
2025-04-21 22:50:04 -0500

    populate endpoint with dataset data

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
-const app = express();
-const dotenv = require('dotenv');
 const path = require('path');
+const app = express();
+
+const dotenv = require('dotenv');
 const exportRouter = require('./routes/export.routes');
 const dataRouter = require('./routes/data.routes');
 const { errorHandler } = require('./middlewares/errorhandler');
 dotenv.config();
 
+// Parse JSON bodies (for API routes)
 app.use(express.json());
 
+// still serve your CSS/images/fonts from “public”
 app.use(express.static('public'));
 
 app.use('/export', exportRouter);
